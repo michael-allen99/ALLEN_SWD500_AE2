6ce0c7cefab10e423a177a8f408fbf000aab4132
Mike <3allem72@solent.ac.uk>
2018-11-29 12:01:57 +0000

    All pages added and base structure complete

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@ import '../sass/main.scss';
 
 //import html pages into bundle
 import '../index.html';
+import '../contact.html';
+import '../uiux.html';
+import '../logos.html';
+import '../websites.html';
+import '../python.html';
 
 //
 import Vue from 'vue';
