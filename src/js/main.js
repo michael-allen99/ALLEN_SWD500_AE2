8d7bac7a75fba08aa42d144bcaab19ec24a1aded
Mike <3allem72@solent.ac.uk>
2018-12-13 20:22:26 +0000

    Javascript analogue clock and image rollover

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import '../uiux.html';
 import '../logos.html';
 import '../websites.html';
 import '../python.html';
+import '../javascript.html';
 
 //import components
 import contact_form from './components/contact_form.vue';
@@ -21,6 +22,31 @@ Vue.use(VeeValidate);
 
 //Set up contact form contact 
 Vue.component('contact-form', contact_form);
+
+
+//START ANALOGUE CLOCK JAVASCRIPT
+const secondSelector = document.querySelector('.seconds'); //set up seconds constant
+const minuteSelector = document.querySelector('.minutes');//set up minutes constant
+const hourSelector = document.querySelector('.hours');//set up hours constant
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds(); //Get seconds
+  const minutes = now.getMinutes(); //Get minutes
+  const hours = now.getHours(); //Get hours
+  const curr_time = hours + ":" + minutes + ":" + seconds; 
+
+  const getSecondsDegrees = ((seconds / 60) * 360); //Work out degrees for seconds
+  const getMinutesDegrees = ((minutes / 60) * 360); //Work out degrees for minutes
+  const getHoursDegrees = ((hours / 60) * 360); //Work out degrees for hours
+
+  secondSelector.style.transform = `rotate(${getSecondsDegrees}deg)`; //Move element to display seconds
+  minuteSelector.style.transform = `rotate(${getMinutesDegrees}deg)`; //Move element to display minutes
+  hourSelector.style.transform = `rotate(${getHoursDegrees}deg)`; //Move element to display hours
+}
+
+setInterval(setDate, 1000); //Update every 1000ms
+//END OF ANALOGUE CLOCK CODE
 
 //Vue searching 
 const app = new Vue({
